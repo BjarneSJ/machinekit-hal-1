d16e37baccbaf2e16f392512d4622cd10ceea490
BjarneS <bjarnesjohansson@gmail.com>
2024-01-07 20:55:45 +0100

    rtapi_app_module.hh

--- src/executables/rtapi_app/src/rtapi_app_module.hh
+++ src/executables/rtapi_app/src/rtapi_app_module.hh
@@ -31,23 +31,23 @@
 class Module
 {
   public:
-    string name;
+    std::string name;
     void *handle;
     char *errmsg;
 
     Module() = default;
     ~Module();
 
-    int load(string module);
-    string path();
+    int load(std::string module);
+    std::string path();
     void clear_err();
-    template <class T> T sym(const string &name);
+    template <class T> T sym(const std::string &name);
     template <class T> T sym(const char *name);
     int elf_section(const char *section_name, void **dest);
     int close();
 };
 
-int Module::load(string module)
+int Module::load(std::string module)
 {
     char module_path[PATH_MAX];
     bool is_rpath;
@@ -56,7 +56,7 @@ int Module::load(string module)
 
     // For module given as a path (sans `.so`), use the path basename as the
     // module name
-    if (module.find_last_of("/") != string::npos) {
+    if (module.find_last_of("/") != std::string::npos) {
         name = module.substr(module.find_last_of("/") + 1);
         dlpath = module + ".so";
         is_rpath = false; // `module` contains `/` chars
@@ -95,7 +95,7 @@ int Module::load(string module)
     return 0;
 }
 
-string Module::path()
+std::string Module::path()
 {
     if (!handle) {
         rtapi_print_msg(RTAPI_MSG_ERR,
